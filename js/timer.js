0f73a0db8afe656a64c4e6c3f0c31e1eed2c2bbd
Roman Isaiev <roma.isaev.2000@gmail.com>
2024-01-05 20:18:47 +0200

    add modal

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   // конечная дата, например 1 июля 2021
-  const deadline = new Date(2023, 11, 1, 8, 0, 0);
+  const deadline = new Date(2024, 2, 1, 8, 0, 0);
   // id таймера
   let timerId = null;
   // склонение числительных
